c212759f1575fb3e5938254a1d07f0368769819f
Chekhov <mdmarkwart@gmail.com>
2026-03-15 20:19:32 +0300

    chore: init book-api priject

---
+++ main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI, HTTPException, status
+from schemas.book import BookCreate
+
+app = FastAPI()
+
+books = {
+    1: {"id": 1, "title": "Dune", "author": "Herbert", "year": 1900},
+    2: {"id": 2, "title": "1984", "author": "Orwell", "year": 1921},
+    3: {"id": 3, "title": "Brave New World", "author": "Huxley", "year": 1921},
+}
+
+
+@app.get("/about")
+def get_about() -> dict:
+    return {
+        "author": "Dima",
+        "project": "books-api"
+    }
+
+
+@app.post("/books", status_code=status.HTTP_201_CREATED)
+def create_book(value: BookCreate) -> dict:
+    book_id = max(books.keys()) + 1
+    book = {"id": book_id, **value.model_dump()}
+    books[book_id] = book
+    return book
+
+
+@app.get("/books")
+def get_books(author: str | None = None, year: int | None = None) -> list[dict]:
+    result = list(books.values())
+    if author is not None:
+        result = [book for book in result if book["author"] == author]
+    if year is not None:
+        result = [book for book in result if book["year"] == year]
+
+    return result
+
+
+@app.get("/books/{book_id}")
+def get_book(book_id: int):
+    if book_id not in books:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found by id")
+    book = books[book_id]
+    return book
